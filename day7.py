d8dc6b2886d73a436c2636b238cdb74356f3372b
Devon <devon.doornbos@gmail.com>
2019-12-14 12:08:58 +0000

    Revert "day5 broken?"
    This reverts commit 11fa114c05c25b0766dcc69418e2094ed9300246.

--- day7.py
+++ day7.py
@@ -1,9 +1,2 @@
 from day5 import computer
 
-inp = [3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0]
-
-def thrusters(L):
-    phases = [0, 1, 2, 3, 4]
-    out = None
-    
-    computer(inp, I1 = phases[0], I2 = 0)
