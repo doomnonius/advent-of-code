100f076f621a620555f551d57a71210e303f7d50
Devon <devon.doornbos@gmail.com>
2020-03-16 19:42:10 -0700

    Solved 10.1, broke while working on 10.2

--- day10.py
+++ day10.py
@@ -1,8 +1,8 @@
 from itertools import permutations
-inp = ".#..#.....#####....#...##"
-h = 5
-# inp = "#.#.###.#.#....#..##.#.........#..#..#..#.#..#.....#.##.##.##.##.##..#...#...##.#...#.#####...###.#.#.#..#####.###.#.#.####.#####.#.#.#.##.#.##...####.#.##.##....###..#.#..#..#..###...##....#.#...##.#.#...####.....#.#######..##.##.#..#.###.#..###.#.#..##.....###.#.#.##.#......#####..###..##.#.##..###.##.###..###..#.###...#.#...#..#.##.#.#..#.#....###.#.#..##.#.##.##.#####..###...#.###.###...##..#..##.##.#.##..####.#.###.###.....####.##..#######....#.##....###.#..#..##.#.####.....###..##.#.#..#..#...#.####..######..#####.##...#.#....#....#.#.#####.##.#.#####..##.#...#..##..##.#.##.##.####..##.##..####..#..####.########.#..#.##.#.######....##...#.##.##.####......#.##.##"
-# h = 26
+# inp = ".#..#.....#####....#...##"
+# h = 5
+inp = "#.#.###.#.#....#..##.#.........#..#..#..#.#..#.....#.##.##.##.##.##..#...#...##.#...#.#####...###.#.#.#..#####.###.#.#.####.#####.#.#.#.##.#.##...####.#.##.##....###..#.#..#..#..###...##....#.#...##.#.#...####.....#.#######..##.##.#..#.###.#..###.#.#..##.....###.#.#.##.#......#####..###..##.#.##..###.##.###..###..#.###...#.#...#..#.##.#.#..#.#....###.#.#..##.#.##.##.#####..###...#.###.###...##..#..##.##.#.##..####.#.###.###.....####.##..#######....#.##....###.#..#..##.#.####.....###..##.#.#..#..#...#.####..######..#####.##...#.#....#....#.#.#####.##.#.#####..##.#...#..##..##.#.##.##.####..##.##..####..#..####.########.#..#.##.#.######....##...#.##.##.####......#.##.##"
+h = 26
 w = 26
 
 def common(L):
@@ -20,16 +20,68 @@ def common(L):
         return True
 
 a = list(filter(lambda x: (x[0] == 1 or (x[0]<x[1] and common(x))) or (x[1] == 1 or (x[1]<x[0] and common(x))), list(permutations(range(1,h), 2)))) #permutations returns a list of lists
-print(a)
+a.append((1,1))
+# print(a)
 
 def count(S, h):
     """Takes a string of asteroid belt data and finds the spot that can see the most asteroids, and how many asteroids it can see.
     """
+    
+    def math_part(point, quadrant, boo, c, z = 0):
+        """ Takes a point, a quadrant and a boolean and figures things out for that quadrant.
+        Quadrants are: RU, RD, LD, LU
+        """
+
+        if boo == True:
+            a.insert(0, (1,0))
+        else:
+            a.insert(0, (0,1))
+
+        if quadrant[1] == "U":
+            m = -1
+        else: m = 1
+
+        if quadrant[0] == "L":
+            n = -1
+        else: n = 1
+        
+        y1, x1 = point[0], point[1]
+
+        # print([y, x])
+        # print(z)
+        # print(a)
+        i, j = a[z][0], a[z][1]
+        # print(i, ",", j)
+        # print(quadrant)
+        while True:
+            if x1 + j*n < w and y1 + m*i < h:
+                print(x1+j*n<w)
+                print([quadrant, c, y+i*m, x+j*n])
+                if complete[y1 + (m*i)][x1 + (j*n)] == "#":
+                    c += 1
+                    if c == 200:
+                        print(x1*100 + y)
+                    if z < len(a)-1: z += 1
+                    else: break
+                    i, j = a[z][0], a[z][1]
+                    # print(i, ",", j)
+                else:
+                    i += a[z][0]
+                    j += a[z][1]
+            else:
+                if z < len(a)-1: z += 1
+                else: break
+                i, j = a[z][0], a[z][1]
+                # print(i, ",", j)
+        a.pop(0)
+        return(c)
+        
+    
     # first I need to split the string into the rows
     total = len(S)
     # print(total)
     w = total/h
-    # print(w)
+    print(w)
     complete = []
     x = 0
     pointer = 0
@@ -43,181 +95,25 @@ def count(S, h):
             pointer += 1
         pointer = 0
         x += 1
-    print(complete)
-    x, y, z, c = 0, 0, 0, 0
+    # print(complete)
+    x, y, c = 0, 0, 0
     results = []
     while y < h:
         while x < w:
             if complete[y][x] == "#":
-                print([y, x])
-                # do the counting thing
-                # note: checking (0, 1) and (1, 1) separately
-                # print(z)
-                i, j = a[z][0], a[z][1]
-                a.append((1,1))
-                a.append((0,1))
-                # right and down from the point
-                while True:
-                    if x + i < w and y + j < h:
-                        if complete[x+i][y+j] == "#":
-                            c += 1
-                            print(["rd1", c, x+i, y+j])
-                            if z < len(a)-1: z += 1
-                            else: break
-                            i, j = a[z][0], a[z][1]
-                        else:
-                            i *= 2
-                            j *= 2
-                    else:
-                        if z < len(a)-1: z += 1
-                        else: break
-                        i, j = a[z][0], a[z][1]
-                z = 0
-                a.pop()
-                a.pop()
-                j, i = a[z][0], a[z][1]
-                while True:
-                    if x + i < w and y + j < h:
-                        if complete[x+i][y+j] == "#":
-                            c += 1
-                            print(["rd2", c, x+i, y+j])
-                            if z < len(a)-1: z += 1
-                            else: break
-                            j, i = a[z][0], a[z][1]
-                        else:
-                            i *= 2
-                            j *= 2
-                    else:
-                        if z < len(a)-1: z += 1
-                        else: break
-                        j, i = a[z][0], a[z][1]
-                z = 0
-                i, j = a[z][0], a[z][1]
-                a.append((1,1))
-                # left and down from the point
-                while True:
-                    if x - i >= 0 and y + j < h:
-                        if complete[x-i][y+j] == "#":
-                            c += 1
-                            print(["ld1", c, x-i, y+j])
-                            if z < len(a)-1: z += 1
-                            else: break
-                            i, j = a[z][0], a[z][1]
-                        else:
-                            i *= 2
-                            j *= 2
-                    else:
-                        if z < len(a)-1: z += 1
-                        else: break
-                        i, j = a[z][0], a[z][1]
-                z = 0
-                a.pop()
-                j, i = a[z][0], a[z][1]
-                a.append((0,1))
-                while True:
-                    if x - i >= 0 and y + j < h:
-                        if complete[x-i][y+j] == "#":
-                            c += 1
-                            print(["ld2", c, x-i, y+j])
-                            if z < len(a)-1: z += 1
-                            else: break
-                            j, i = a[z][0], a[z][1]
-                        else:
-                            i *= 2
-                            j *= 2
-                    else:
-                        if z < len(a)-1: z += 1
-                        else: break
-                        j, i = a[z][0], a[z][1]
-                z = 0
-                a.pop()
-                i, j = a[z][0], a[z][1]
-                a.append((1,1))
-                a.append((0,1))
-                # left and up from the point
-                while True:
-                    if x - i >= 0 and y - j >= 0:
-                        if complete[x-i][y-j] == "#":
-                            c += 1
-                            print(["lu1", c, x-i, y-j])
-                            if z < len(a)-1: z += 1
-                            else: break
-                            i, j = a[z][0], a[z][1]
-                        else:
-                            i *= 2
-                            j *= 2
-                    else:
-                        if z < len(a)-1: z += 1
-                        else: break
-                        i, j = a[z][0], a[z][1]
-                z = 0
-                a.pop()
-                a.pop()
-                j, i = a[z][0], a[z][1]
-                while True:
-                    if x - i >= 0 and y - j >= 0:
-                        if complete[x-i][y-j] == "#":
-                            c += 1
-                            print(["lu2", c, x-i, y-j])
-                            if z < len(a)-1: z += 1
-                            else: break
-                            j, i = a[z][0], a[z][1]
-                        else:
-                            i *= 2
-                            j *= 2
-                    else:
-                        if z < len(a)-1: z += 1
-                        else: break
-                        j, i = a[z][0], a[z][1]
-                z = 0
-                a.append((1,1))
-                i, j = a[z][0], a[z][1]
-                # right and up from the point
-                while True:
-                    if x + i < w and y - j >= 0:
-                        if complete[x+i][y-j] == "#":
-                            c += 1
-                            print(["ru1", c, x+i, y-j])
-                            if z < len(a)-1: z += 1
-                            else: break
-                            i, j = a[z][0], a[z][1]
-                        else:
-                            i *= 2
-                            j *= 2
-                    else:
-                        if z < len(a)-1: z += 1
-                        else: break
-                        i, j = a[z][0], a[z][1]
-                z = 0
-                a.pop()
-                j, i = a[z][0], a[z][1]
-                a.append((0,1))
-                while True:
-                    if x + i < w and y - j >= 0:
-                        if complete[x+i][y-j] == "#":
-                            c += 1
-                            print(["ru2", c, x+i, y-j])
-                            if z < len(a)-1: z += 1
-                            else: break
-                            j, i = a[z][0], a[z][1]
-                        else:
-                            i *= 2
-                            j *= 2
-                    else:
-                        if z < len(a)-1: z += 1
-                        else: break
-                        j, i = a[z][0], a[z][1]
+                c = math_part((y,x), "RU", True, c)
+                c = math_part((y,x), "RD", False, c)
+                c = math_part((y,x), "LD", True, c)
+                c = math_part((y,x), "LU", True, c)
                 # add count to results table
-                a.pop()
-                results.append([c, (y, x)])
+                results.append(c)
                 c = 0
-                z = 0
             x += 1 # this increments the check for clear spaces
         x = 0
         y += 1
     
     # results.sort(key=lambda x: x[1])
-    return results
+    return max(results)
 
 
 # def los(S):
