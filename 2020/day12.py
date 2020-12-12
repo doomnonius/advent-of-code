1f9902fe1a40b0b526ee591cba696591f6905280
doomnonius <devon.doornbos@gmail.com>
2020-12-11 22:16:07 -0800

    updates to day 12

--- 2020/day12.py
+++ 2020/day12.py
@@ -18,32 +18,21 @@ def manhattan(instructions):
 	
 	
 	def move_ship(location, number, rise_run):
-		location[0] += rise_run[0]*number
-		location[1] += rise_run[1]*number
-		return location
+		return [location[0] + rise_run[0]*number, location[1] + rise_run[1]*number]
 
-	ins = []
 	start = [0,0]
 	waypoint = [1,10]
-	headings = ["N", "E", "S", "W"]
-	heading = "E"
 	for x in instructions:
-		x = Instruction(x)
-		ins.append(x)
-	for x in ins:
-		if x.letter in headings:
-			waypoint = move_waypoint(x.letter, x.number, waypoint)
-		elif x.letter == "R":
+		if x.letter == "R":
 			for i in range(x.number//90):
 				waypoint = [-waypoint[1], waypoint[0]]
-			# print(f"waypoint: {waypoint}")
 		elif x.letter == "L":
 			for i in range(x.number//90):
 				waypoint = [waypoint[1], -waypoint[0]]
-			# print(f"waypoint: {waypoint}")
 		elif x.letter == "F":
 			start = move_ship(start, x.number, waypoint)
-		# print(start)
+		else:
+			waypoint = move_waypoint(x.letter, x.number, waypoint)
 
 	return abs(start[0]) + abs(start[1])
 
@@ -54,11 +43,6 @@ if __name__ == "__main__":
 	FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 	with open(os.path.join(FILE_DIR, "day12.input")) as f:
 		DATA = f.read().strip()
-	TESTCASE = """F10
-N3
-F7
-R90
-F11"""
-	INSTRUCTIONS = DATA.split("\n")
+	INSTRUCTIONS = [Instruction(line) for line in DATA.split("\n")]
 	# print(f"Part one: {manhattan(INSTRUCTIONS)}")
-	print(f"Part two: {manhattan(INSTRUCTIONS)}") # not 19011, or 35353, or 35531
+	print(f"Part two: {manhattan(INSTRUCTIONS)}") # not 19011, or 35353, or 35531, but is 63447
