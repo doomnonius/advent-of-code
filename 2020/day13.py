681316d832b8a303f6682b3a1be3f4d7a6441f94
doomnonius <devon.doornbos@gmail.com>
2020-12-12 22:35:46 -0800

    solved day13 p1

---
+++ 2020/day13.py
@@ -0,0 +1,56 @@
+
+class Bus:
+	def __init__(self, number, position):
+		try:
+			self.id = int(number)
+			self.departures = {(self.id*(1000000//self.id))+position}
+		except:
+			self.id = number
+			self.departures = {1000000}#100000000000000}
+
+
+	
+	def next_time(self):
+		if type(self.id) == int:
+			self.departures.add(max(self.departures)+self.id)
+		else:
+			self.departures.add(max(self.departures)+1)
+
+def next_bus(arrival, buses):
+	all_nearest = {}
+	for bus in buses:
+		if type(bus.id) == str:
+			continue
+		next_dep = 0
+		while next_dep < arrival:
+			next_dep += bus.id
+		all_nearest[next_dep] = bus.id
+	minimum = min(all_nearest.keys())
+	return (minimum-arrival)*all_nearest[minimum]
+			
+def subsequent_departures(buses):
+	deps = [bus.departures for bus in buses]
+	while (retVal := set.intersection(*deps)) == set():
+		# I don't know how I am going to do this, but I want to find the smallest one and increment that one
+		for bus in buses:
+			bus.next_time()
+	return retVal
+	
+
+TEST_DATA = """939
+7,13,x,x,59,x,31,19"""
+
+if __name__ == "__main__":
+	import os
+	FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+	with open(os.path.join(FILE_DIR, "day13.input")) as f:
+		DATA = f.read().strip()
+	DATA = TEST_DATA.split("\n")
+	ARRIVAL, SCHEDULE = int(DATA[0]), DATA[1]
+	BUSES = []
+	SCHED = SCHEDULE.split(",")
+	for i in range(L := len(SCHED)):
+		# print(f"{i}")
+		BUSES.append(Bus(SCHED[i], L-1-i))
+	print(f"Part one: {next_bus(ARRIVAL, BUSES)}")
+	print(f"Part two: {subsequent_departures(BUSES)}")
