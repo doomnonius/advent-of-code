01ca0c34f870010ddba0ecbcdf910a1ae3418fee
Devon Doornbos <devon_doornbos@selinc.com>
2020-12-14 16:54:23 -0800

    small changes to day 13 p2

--- 2020/day13.py
+++ 2020/day13.py
@@ -1,19 +1,11 @@
 
 class Bus:
 	def __init__(self, number, position):
-		try:
-			self.id = int(number)
-			self.departures = {(self.id*(100000000000000//self.id))+position}
-		except:
-			self.id = number
-			self.departures = {100000000000000}
-
+		self.id = int(number)
+		self.departures = (self.id*(100000000000000//self.id))+position
 
 	def next_time(self):
-		if type(self.id) == int:
-			self.departures.add(max(self.departures)+self.id)
-		else:
-			self.departures.add(max(self.departures)+1)
+		self.departures += self.id
 
 def next_bus(arrival, buses):
 	all_nearest = {}
@@ -28,20 +20,26 @@ def next_bus(arrival, buses):
 	return (minimum-arrival)*all_nearest[minimum]
 			
 def subsequent_departures(buses, num_buses: int):
-	deps = [bus.departures for bus in buses]
-	while (retVal := set.intersection(*deps)) == set():
+	deps = {bus.departures for bus in buses}
+	counter = 1
+	if max(deps) > 100000000000000 + (1000000000 * counter):
+		print(f"{counter} billion")
+		print(datetime.datetime.now())
+		counter += 1
+	while len(deps) > 1:
 		# I don't know how I am going to do this, but I want to find the smallest one and increment that one
 		# What I currently have going on is just incrementing all of them
 		for bus in buses:
-			bus.next_time()
-	return sum(retVal) - (num_buses - 1)
+			if bus.departures < max(deps):
+				bus.next_time()
+	return sum(deps) - (num_buses - 1)
 	
 
 TEST_DATA = """939
 7,13,x,x,59,x,31,19"""
 
 if __name__ == "__main__":
-	import os
+	import os, datetime
 	FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 	with open(os.path.join(FILE_DIR, "day13.input")) as f:
 		DATA = f.read().strip()
@@ -54,4 +52,6 @@ if __name__ == "__main__":
 		if SCHED[i] != 'x':
 			BUSES.append(Bus(SCHED[i], L-1-i))
 	print(f"Part one: {next_bus(ARRIVAL, BUSES)}")
+	print(datetime.datetime.now())
 	print(f"Part two: {subsequent_departures(BUSES, L)}")
+	print(datetime.datetime.now())
