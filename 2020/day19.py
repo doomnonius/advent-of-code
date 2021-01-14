d9926eb1bd95427ecd70a68fb1a255d4fccefa6b
Devon Doornbos <devon_doornbos@selinc.com>
2021-01-14 14:52:58 -0800

    stole peter's day 19; still dont understand regex

--- 2020/day19.py
+++ 2020/day19.py
@@ -1,44 +1,31 @@
-import re, itertools
+import re
 from typing import Dict, List
 # first thing to do is break down the rules
 # then convert them all to regex expressions
-class Rule:
-	def __init__(self, inp: str): # 109: 128 16 | 90 68 # '128: "b"'
-		self.id, rule = inp.split(": ")
-		self.group1, self.group2 = rule.split(" | ")
-		pattern = r"(\d+): \"?(\w+)\"? ?(\d{0,3})( \| (\d+) (\d{0,3}))?"
-		match = re.match(pattern, inp)
-		if match:
-			self.id = int(match.group(1))
-			self.pattern = None
-			if 'b' in match.group(2) or 'a' in match.group(2):
-				self.base = True
-			else: self.base = False
-			if not self.base: self.group1 = [int(match.group(2)), int(match.group(3))]
-			else: self.group1 = match.group(2)
-			self.either = not (not match.group(4))
-			if self.either:
-				self.group2 = [int(match.group(5)), int(match.group(6))]
-			else: self.group2 = []
-		else:
-			raise Exception(f"Failed to parse line: {inp}")
 
-def rule_to_regex(rules_hash: Dict[int, List[List]], rule):
-	string = r""
-	for pair in rules_hash[rule]:
-		if type(pair) != str and pair: # ie not an empty list
-			for sub_rule in pair:
-				string += itertools.combinations([rule_to_regex(rules_hash, sub_rule)])
-		else:
-			return string
-	
+def build_rule(rules_dict, current_rule, depth=0):
+	sections = []
+	for section in rules_dict[current_rule]:
+		section_regex = ""
+		for label in section:
+			if '"' in label: # this catches the letters
+				section_regex += label[1]
+			else:
+				if label == current_rule:
+					if depth < 5:
+						section_regex += build_rule(rules_dict, label, depth=depth+1)
+				else:
+					section_regex += build_rule(rules_dict, label, depth=depth)
+		sections.append(section_regex)
+	regex_str = f"{'|'.join(sections)}" if len(sections) > 1 else sections[0]
+	print(regex_str)
+	return regex_str if len(regex_str) == 1 else f"({regex_str})"
 
-
-def match_rule(message: str, rule: int, rules: Dict) -> str:
-	pat = rule_to_regex(rule, rules)
-	print(pat)
-	return message
-
+def parse_messages(rules_dict, messages):
+	starting_rule = build_rule(rules_dict, "0")
+	regex = re.compile(f"^{starting_rule}$")
+	print(regex)
+	return sum(1 for m in messages if regex.match(m))
 
 TEST_DATA="""0: 4 1 5
 1: 2 3 | 3 2
@@ -58,10 +45,16 @@ if __name__ == "__main__":
 	FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 	with open(os.path.join(FILE_DIR, "day19.input")) as f:
 		DATA = f.read().strip()
-	RULES, MESSAGES = TEST_DATA.split("\n\n")
-	RULES = {y.id: [y.group1, y.group2] for y in [Rule(x) for x in RULES.split("\n")]}
-	print(RULES)
+	RULES, MESSAGES = DATA.split("\n\n")
+	# RULES = {y.id: [y.group1, y.group2] for y in [Rule(x) for x in RULES.split("\n")]}
+	RULES: Dict[str, List[List[str]]] = {
+		num: [section.split() for section in right.split("|")]
+		for line in RULES.split("\n")
+		for num, right in [line.split(": ")]
+	}
 	MESSAGES = MESSAGES.split("\n")
-	print(f"Part one: {len([match_rule(message, 0, RULES) for message in MESSAGES])}")
-	# print(f"Part two: {}")
+	print(f"Part one: {parse_messages(RULES, MESSAGES)}")
+	# RULES['8'] = [['42'], ['42', '8']]
+	# RULES['11'] = [['42', '31'], ['42', '11', '31']]
+	# print(f"Part two: {parse_messages(RULES, MESSAGES)}")
 	# print(f"Time: {timeit.timeit('', setup='from __main__ import ', number = 1)}")
