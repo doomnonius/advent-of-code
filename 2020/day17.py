f7bd2293edb45861ac382f9739815c7520fbd215
Devon Doornbos <devon_doornbos@selinc.com>
2021-01-14 12:57:48 -0800

    solved day 17 as per method used in day 24

--- 2020/day17.py
+++ 2020/day17.py
@@ -1,63 +1,64 @@
-from typing import Dict, List, NamedTuple
+from typing import Dict, List, NamedTuple, Set
 from itertools import combinations, product
 
+## Don't need to make a whole plane, with empty spots 'n shit. Can do like I did in day 24.
+
+class Coord3d(NamedTuple):
+	x: int
+	y: int
+	z: int
+
+	def neighbors(self):
+		return {Coord3d(x[0], x[1], x[2]) for x in  product([-1, 0, 1], repeat=3) if x != (0, 0, 0)}
+	
+	def __add__(self, other):
+		return Coord3d(self.x + other.x, self.y + other.y, self.z + other.z)
+	
+	def __repr__(self):
+		return "(" + str(self.x) + ',' + str(self.y) + ',' + str(self.z) + ")"
+
+class Coord4d(NamedTuple):
+	x: int
+	y: int
+	z: int
+	t: int
+
+	def neighbors(self):
+		return {Coord4d(x[0], x[1], x[2], x[3]) for x in  product([-1, 0, 1], repeat=4) if x != (0, 0, 0, 0)}
+	
+	def __add__(self, other):
+		return Coord4d(self.x + other.x, self.y + other.y, self.z + other.z, self.t + other.t)
+	
+	def __repr__(self):
+		return "(" + str(self.x) + ',' + str(self.y) + ',' + str(self.z) + ',' + str(self.t) + ")"
+
 class Space:
-	def __init__(self, initial_state):
+	def __init__(self, initial_state: Set[Coord3d]):
 		""" Note: 1 to 3 and -1 to -3 are mirrors.
 		"""
 		self.layout = initial_state
-		self.dim = {"z": len(self.layout), "y": len(self.layout[0]), "x": len(self.layout[0][0])}
-
-	def generate_layer(self):
-		new_plane = []
-		while len(new_plane) < self.dim["y"]:
-			new_line = [0] * self.dim["x"]
-			new_plane.append(new_line)
-		self.layout[self.dim["z"]] = new_plane
-		self.dim["z"] += 1
-		return self
-	
-
-	def extend_layer(self):
-		self.dim["x"] += 2
-		new_line = [0] * self.dim["x"]
-		# print(f"y: {self.dim['y']}")
-		for key in self.layout.keys():
-			for i in range(self.dim["y"]):
-				self.layout[key][i] = [0] + self.layout[key][i]
-				self.layout[key][i].append(0)
-			# print(f"e1: {self.layout}")
-			self.layout[key] = [new_line] + self.layout[key]
-			# print(f"e2: {self.layout}")
-			self.layout[key].append(new_line)
-		self.dim["y"] += 2
-		return self
-
 
 	def permutate(self):
-		broken = False
-		self.generate_layer()
-		print("1: ", self.layout)
-		for key in self.layout.keys():
-			for coords in [c for c in combinations(range(self.dim["x"]), 2) if (0 in c or self.dim["x"]-1 in c)]:
-				if self.layout[key][coords[0]][coords[1]] == 1:
-					self.extend_layer() # this will always return a square
-					broken = True
-					break
-			if broken: break
-		if sum(sum(L) for L in self.layout[self.dim["z"]-2]) > 0:
-			self.generate_layer()
-		print("2: ", self.layout)
-		## This is where the main code body will be
-		next_state = self.layout.copy()
-		for z in range(len(self.layout)-2):
-			for y in range(1, len(self.layout[z])-1):
-				for x in range(1, len(self.layout[z][y])-1):
-					if self.layout[z][y][x] == 1 and self.count_neighbors((x, y, z)) not in [2, 3]:
-						next_state[z][y][x] = 0
-					elif self.layout[z][y][x] == 0 and self.count_neighbors((x, y, z)) == 3:
-						next_state[z][y][x] = 1
-		self.layout = next_state
+		old_layout = self.layout.copy()
+		for point in old_layout:
+			n = {point+x for x in point.neighbors()}
+			n_count = 0
+			for pn in n:
+				# count active neighbors
+				if pn in old_layout:
+					n_count += 1
+				# check if any become active
+				else:
+					n2 = {pn + y for y in pn.neighbors()}
+					n2_count = 0
+					for pn2 in n2:
+						if pn2 in old_layout:
+							n2_count += 1
+					if n2_count == 3:
+						self.layout.add(pn)
+			# check if any become inactive
+			if n_count not in [2, 3]:
+				self.layout.remove(point)
 		return self
 
 	
@@ -67,45 +68,31 @@ class Space:
 			count -= 1
 		return self
 
-
-	def count_neighbors(self, coord: tuple):
-		neighbors = 0
-		for x in product([-1, 0, 1], repeat=3):
-			if x != (0, 0, 0):
-				z = abs(coord[2] + x[2])
-				# print(f"x,y,z: {coord[0]+x[0], coord[1]+x[1], z}")
-				if self.layout[z][coord[1]+x[1]][coord[0]+x[0]] == 1:
-					neighbors += 1
-		return neighbors
-
 	def count(self):
-		c = 0
-		for key in self.layout.keys():
-			x = sum(sum(L) for L in self.layout[key])
-			if key != 0: x *= 2
-			c += x
-		return c
+		return len(self.layout)
 
 
 TEST_DATA=""".#.
 ..#
-###""" # should return 112
+###""" # should return 112 for p1, 848 for p2
 
 if __name__ == "__main__":
 	import os, timeit
 	FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 	with open(os.path.join(FILE_DIR, "day17.input")) as f:
 		DATA = f.read().strip()
-	ROWS = TEST_DATA.split("\n")
-	PLANE = {0: []}
-	for i in range(len(ROWS)):
-		PLANE[0].append([])
-		for char in ROWS[i]:
-			if char == ".":
-				PLANE[0][i].append(0)
-			else:
-				PLANE[0][i].append(1)
-	print(PLANE)
+	ROWS = DATA.split("\n")
+	PLANE = set()
+	PLANE2 = set()
+	for row in range(len(ROWS)):
+		for char in range(len(ROWS[row])):
+			if ROWS[row][char] == "#":
+				PLANE.add(Coord3d(row, char, 0))
+	for row in range(len(ROWS)):
+		for char in range(len(ROWS[row])):
+			if ROWS[row][char] == "#":
+				PLANE2.add(Coord4d(row, char, 0, 0))
+	print(PLANE2)
 	print(f"Part one: {Space(PLANE).run(6).count()}")
-	# print(f"Part two: {}")
+	print(f"Part two: {Space(PLANE2).run(6).count()}")
 	# print(f"Time: {timeit.timeit('', setup='from __main__ import ', number = 1)}")
