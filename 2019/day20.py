8e93dc188a40a46cf0c7f81010ef6d393263e01c
Devon Doornbos <devon_doornbos@selinc.com>
2021-02-02 09:12:50 -0800

    start day 20

---
+++ 2019/day20.py
@@ -0,0 +1,12 @@
+
+
+
+if __name__ == "__main__":
+	import os, timeit
+	FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+	with open(os.path.join(FILE_DIR, "day20.input")) as f:
+		DATA = f.read().strip()
+	DATA = DATA.split("\n")
+	print(f"Part one: {DATA}")
+	# print(f"Part two: {}")
+	# print(f"Time: {timeit.timeit('', setup='from __main__ import ', number = 1)}")
