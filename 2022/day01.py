006808800483c19b61f58e78059bdc89e2eb4216
doomnonius <devon.doornbos@gmail.com>
2022-11-30 21:11:34 -0800

    solved day 1

--- 2022/day01.py
+++ 2022/day01.py
@@ -1,24 +1,31 @@
 from typing import List
 
-def part1(nums: List[int]) -> int:
-    return
+def part1(nums: List[List[str]]) -> int:
+    e_max = 0
+    for elf in nums:
+        e_total = sum(int(x) for x in elf)
+        if e_total > e_max:
+            e_max = e_total
+    return e_max
 
-
-
-
-
-def part2(nums: List[int]) -> int:
-    return
+def part2(nums: List[List[str]]) -> int:
+    e_max = [0,0,0]
+    for elf in nums:
+        e_total = sum(int(x) for x in elf)
+        if e_total > min(e_max):
+            e_max[e_max.index(min(e_max))] = e_total
+    return sum(e_max)
 
 
 
 
 if __name__ == "__main__":
-	import os, timeit
-	FILE_DIR = os.path.dirname(os.path.abspath(__file__))
-	file = os.path.splitext(__file__)[0][-5:]
-	with open(os.path.join(FILE_DIR, file + ".input")) as f:
-		DATA = f.read().strip()
-	DATA = [int(x) for x in DATA.split()] # example code
-	print(f"Part 1: {part1(DATA)}")
-	print(f"Part 2: {part2(DATA)}")
+    import os, timeit
+    FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+    file = os.path.splitext(__file__)[0][-5:]
+    with open(os.path.join(FILE_DIR, file + ".input")) as f:
+        DATA = f.read().strip()
+    DATA = [x.split() for x in DATA.split("\n\n")] # example code
+    # print(DATA)
+    print(f"Part 1: {part1(DATA)}")
+    print(f"Part 2: {part2(DATA)}")
