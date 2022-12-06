c684a17eca65b7fd3129738fd4e3032d74ba3d73
doomnonius <devon.doornbos@gmail.com>
2022-12-05 21:17:35 -0800

    solved day 6

---
+++ 2022/day06.py
@@ -0,0 +1,35 @@
+from typing import List
+
+def part1(inp: str) -> int:
+    i = 4
+    while i < len(inp):
+        if len(set(inp[i-4:i])) == 4:
+            return i
+        i += 1
+        
+def part2(inp: str) -> int:
+    i = 14
+    while i < len(inp):
+        if len(set(inp[i-14:i])) == 14:
+            return i
+        i += 1
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    import os, timeit
+    FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+    file = os.path.splitext(__file__)[0][-5:]
+    with open(os.path.join(FILE_DIR, file + ".input")) as f:
+        DATA = str(f.read().strip())
+    # DATA = [int(x) for x in DATA.split()] # example code
+    print(f"test: {part1('mjqjpqmgbljsphdztnvjfqwrcgsmlb')}")
+    print(f"Part 1: {part1(DATA)}")
+    print(f"Part 2: {part2(DATA)}")
