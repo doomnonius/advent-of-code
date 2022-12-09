8452cba62a26ebd0449f4511be13bdaa1e473236
doomnonius <devon.doornbos@gmail.com>
2022-12-08 16:39:22 -0800

    simplified 2021 day 9

--- 2021/day09.py
+++ 2021/day09.py
@@ -1,41 +1,27 @@
-from typing import List, NamedTuple
-
-class Coord(NamedTuple):
-    x: int
-    y: int
-    val: int
-
-    def __add__(self, other):
-        return Coord(self.x + other.x, self.y + other.y, self.val)
+from typing import List, Dict, Tuple
     
-    def neighbors(self):
-        return [self + Coord(0, 1, 0), self + Coord(0, -1, 0), self + Coord(1, 0, 0), self + Coord(-1, 0, 0)]
+def neighbors(t: Tuple):
+    x, y = t
+    return [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
 
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
-
-    def __hash__(self):
-        return hash((self.x, self.y))
-
-def part1(nums: List[Coord]) -> int:
+def part1(nums: Dict[Tuple[int,int], int]) -> int:
     retVal = 0
     bad = False
-    for pt in nums:
-        for n in pt.neighbors():
-            if n in nums:
-                if test: print(f"{nums[nums.index(n)].val}")
-                if nums[nums.index(n)].val < pt.val:
+    for pt in nums.keys():
+        for n in neighbors(pt):
+            try:
+                if nums[n] <= nums[pt]:
                     bad = True
                     break
+            except KeyError:
+                if test: print(f"key error") # nothing
         if not bad:
-            retVal += pt.val + 1
-            bad = False
-        else:
-            bad = False
-            continue
+            if test: print(f"pt: {pt} adding: {nums[pt] + 1}")
+            retVal += nums[pt] + 1
+        bad = False
     return retVal
 
-def part2(co: List[Coord]) -> int:
+def part2(nums: Dict[Tuple[int,int], int]) -> int:
     return
 
 
@@ -49,12 +35,12 @@ if __name__ == "__main__":
     else: INPUT_FILE = Path(__file__).with_suffix(".input")
     DATA = INPUT_FILE.read_text().strip().split()
     # print(DATA)
-    COORDS = []
+    COORDS = {}
     y = 0
     for row in DATA:
         x = 0
         while x < len(row):
-            COORDS.append(Coord(x, y, int(row[x])))
+            COORDS[(x, y)] = int(DATA[y][x])
             x += 1
         y += 1
     # print(COORDS)
