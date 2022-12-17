0e2b3a81a79d3aa41b162af55ba4f92b41836eee
Devon Doornbos <devon.doornbos@gmail.com>
2022-12-16 16:44:29 -0800

    solved day 12

--- 2022/day12.py
+++ 2022/day12.py
@@ -1,59 +1,84 @@
-from typing import List, Dict, Tuple, Set
+from typing import List, Tuple, Dict
 import string
 
-def neighbors(t: Tuple):
+class Nav:
+    def __init__(self, pos: Tuple, turn: Tuple):
+        self.pos = pos
+        self.face = turn
+        self.turn_right = {
+            (1, 0): (0, 1),
+            (0, 1): (-1, 0),
+            (-1, 0): (0, -1),
+            (0, -1): (1, 0)
+        }
+        self.turn_left = {
+            (1, 0): (0, -1),
+            (0, 1): (1, 0),
+            (-1, 0): (0, 1),
+            (0, -1): (-1, 0)
+        }
+        self.path = [pos]
+
+def neighbors(t: Tuple[int, int]) -> List[Tuple[int, int]]:
     x, y = t
     return [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
 
-def part1(coords: Dict[Tuple, str]) -> int:
-    # thoughts: go through and map the spots that have neighbors that they can move to
-    # then work from there somehow
-    # maybe I can find a quick way to list all possible paths with this info and find the shortest?
+def part1(coords: Dict[Tuple, str], start: Tuple, end: Tuple) -> int:
+    # now we'll try a point map
     adj = {x:[] for x in coords.keys()}
-    start = [k for k,v in coords.items() if v == 44][0]
-    end = [k for k,v in coords.items() if v == 30][0]
-    for k in coords.keys():
-        if coords[k] == 44:
-            coords[k] = 0
-        if coords[k] == 30:
-            coords[k] = 25
+    dist = {x:1000 for x in coords.keys()}
+    dist[start] = 0
+    if test: print(f"start: {start}")
+    # find the neighbors of each spot
     for c in coords.keys():
         for n in neighbors(c):
             if n not in coords.keys():
                 continue
             if coords[n] <= (coords[c] + 1):
                 adj[c].append(n)
-    # if test: print([k for k,v in adj.items() if len(v) == 2])
-    r = recursion(adj, start, end, set([start]))
-    if test: print(f"r: {r}")
-    return min([len(x) for x in r])
-
-def recursion(near: Dict[Tuple, List[Tuple]], s: Tuple, e: Tuple, seen: Set[Tuple]): # can I even remember how to write a recursive function?
-    # if test: print(f"seen: {seen}")
-    returned = []
-    for n in near[s]:
-        if test and s == (0,0): print(n)
-        if n in seen:
-            continue
-        if n == e:
-            print("found the end!")
-            if test: print(f"seen after found: {seen}")
-            returned.append(seen)
-        seen.add(n)
-        returning = recursion(near, n, e, seen.copy())
-        if returning and returning not in returned:
-            # if test: print(returning)
-            returned.extend(returning)
-    # if test: print(returned)
-    return returned
+    # now find the distant from the start for each spot - will this get computationally out of hand?
+    to_do = [(adj[start], 1)]
+    while to_do:
+        now = to_do.pop()
+        for n in now[0]:
+            if dist[n] > now[1]:
+                dist[n] = now[1]
+                next = adj[n].copy()
+                if n in next: next.remove(n)
+                to_do.append((next, now[1] + 1))
+    if test: print(f"distances: {dist}")
+    return(dist[end])
 
 
-
-def part2(nums: List[int]) -> int:
-    return
-
-
-
+def part2(coords: Dict[Tuple, str], end: Tuple) -> int:
+    adj = {x:[] for x in coords.keys()}
+    dist = {x:1000 for x in coords.keys()}
+    starts = [k for k,v in coords.items() if v == 0]
+    # find the neighbors of each spot
+    for c in coords.keys():
+        for n in neighbors(c):
+            if n not in coords.keys():
+                continue
+            if coords[n] <= (coords[c] + 1):
+                adj[c].append(n)
+    # now find the distant from the start for each spot - will this get computationally out of hand?
+    retVal = 1000
+    for start in starts:
+        if test: print(f"start: {start}")
+        dist[start] = 0
+        to_do = [(adj[start], 1)]
+        while to_do:
+            now = to_do.pop()
+            for n in now[0]:
+                if dist[n] > now[1]:
+                    dist[n] = now[1]
+                    next = adj[n].copy()
+                    if n in next: next.remove(n)
+                    to_do.append((next, now[1] + 1))
+        if test: print(f"distances: {dist}")
+        if dist[end] < retVal:
+            retVal = dist[end]
+    return retVal
 
 if __name__ == "__main__":
     import os, timeit
@@ -66,6 +91,11 @@ if __name__ == "__main__":
     for y in range(len(DATA)):
         for x in range(len(DATA[y])):
             COORDS[(x,y)] = string.ascii_letters.index(DATA[y][x])
-    # if test: print(COORDS)
-    print(f"Part 1: {part1(COORDS)}")
-    print(f"Part 2: {part2(COORDS)}")
+    # if test: print(f"coords: {COORDS}")
+    start = [k for k,v in COORDS.items() if v == 44][0]
+    end = [k for k,v in COORDS.items() if v == 30][0]
+    for k in COORDS.keys():
+        if COORDS[k] == 44: COORDS[k] = 0
+        if COORDS[k] == 30: COORDS[k] = 25
+    print(f"Part 1: {part1(COORDS, start, end)}") # not 337
+    print(f"Part 2: {part2(COORDS, end)}")
