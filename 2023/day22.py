c7d7137ce5e7696d3e8fff2efa0d97cdf80c7e28
Devon <devon.doornbos@emsibg.com>
2023-11-30 21:40:27 -0800

    added blanks for 2023

---
+++ 2023/day22.py
@@ -0,0 +1,24 @@
+from typing import List
+
+def part1(nums: List[int]) -> int:
+    return
+
+
+
+
+
+def part2(nums: List[int]) -> int:
+    return
+
+
+
+
+if __name__ == "__main__":
+	import os, timeit
+	from pathlib import Path
+	INPUT_FILE = Path(__file__).with_suffix(".input")
+	DATA = INPUT_FILE.read_text().strip()
+	DATA = [int(x) for x in DATA.split()] # example code
+	print(f"Part 1: {part1(DATA)}")
+	print(f"Part 2: {part2(DATA)}")
+    
