8162aa0093a3b338a58919cfb3628edfc8730a67
Devon <devon.doornbos@emsibg.com>
2023-12-02 23:02:53 -0800

    solved 2023.3.1

--- 2023/day03.py
+++ 2023/day03.py
@@ -1,7 +1,7 @@
-from typing import List, Dict
+from typing import Tuple, List
 from string import digits
 
-def process_data(raw_data:str) -> Dict[int, str]:
+def process_data(raw_data:str) -> List[Tuple[int, str]]:
     """ the dictionary key will be the part number
         the dictionary value will be the adjacent symbols (full list)
     """
@@ -71,7 +71,7 @@ def process_data(raw_data:str) -> Dict[int, str]:
             pass
         return s
 
-    r = dict()
+    r = []
     x = y = 0
     while y < len(data):
         while x < len(data[y]):
@@ -87,23 +87,23 @@ def process_data(raw_data:str) -> Dict[int, str]:
                 v *= 10
                 v += int(data[y][x])
                 n += neighbor_symbols(x, y)
-                print(f"number: {v}, symbols: {n}")
-                _ = input("pause to verify")
+                # print(f"number: {v}, symbols: {n}")
+                # _ = input("pause to verify")
                 # Warning! this will count symbols more than once (if adjacent to multiple numbers) NB for p2
                 x += 1
             if v > 0:
-                r[v] = n
+                r.append((v,n))
             x += 1
         x = 0
         y += 1
     return r
 
-def part1(data: Dict[int, str]) -> int:
+def part1(data: List[Tuple[int, str]]) -> int:
     t = 0
-    for k,v in data.items():
-        print(f"number: {k}, symbols: {v}")
-        if len(v) > 0:
-            t += k
+    for i in data:
+        # print(f"number: {i[0]}, symbols: {i[1]}")
+        if len(i[1]) > 0:
+            t += i[0]
     return t
 
 
@@ -122,5 +122,5 @@ if __name__ == "__main__":
     INPUT_FILE = Path(__file__).with_suffix(".input")
     DATA = INPUT_FILE.read_text().strip()
     DATA = process_data(DATA)
-    print(f"Part 1: {part1(DATA)}") # 308893 too low
+    print(f"Part 1: {part1(DATA)}") # 308893 too low; solution didn't account for a number showing up more than once
     print(f"Part 2: {part2(DATA)}")
