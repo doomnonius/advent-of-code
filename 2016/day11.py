ce53c13571cc0fe6480ac4549f5c1d4ac3224c9a
Devon <devon.doornbos@emsibg.com>
2023-12-04 21:59:07 -0800

    trying to optimize 2016.d11.p2

--- 2016/day11.py
+++ 2016/day11.py
@@ -1,16 +1,17 @@
-from typing import Dict, List
+from typing import Dict, List, Tuple
 import itertools
 
-def travel(floors: Dict, floor: int) -> int:
-    # check = floors["E"].count
-    # if (check > 2) or (check < 1):
-    pass
-        
-def check_valid(floors: Dict, expected:int) -> bool:
-    count = len(floors[4]) + len(floors[3]) + len(floors[2]) + len(floors[1])
-    if count != expected:
-        print(f"Error! Wrong number of items ({count})[12]!")
-        return 9999
+def hash_floors(floors: Dict) -> Tuple[str]:
+    t = []
+    for i in range(1,5):
+        p = sorted(floors[i].copy())
+        if floors["E"] == i:
+            p.append("E")
+        t.append(tuple(p))
+    # print(tuple(t))
+    return tuple(t)
+
+def check_valid(floors: Dict) -> bool:
     for i in range(1,5):
         p = floors[i] # present
         generators = {g for g in p if g[1] == "G"}
@@ -20,55 +21,28 @@ def check_valid(floors: Dict, expected:int) -> bool:
                     return False
     return True
 
-def part1(floors: Dict, steps:int = 0, test:bool = False) -> int:
+def check_ele(pair: List[str]) -> bool:
+    if pair[0][1] != pair[1][1]:
+        if pair[0][0] != pair[1][0]:
+            return False
+    return True
+
+# establishing these outside of the functions so both functions can access them
+heap = []
+hist = set()
+heap_length = []
+
+def move_elevator(floors:Dict,count:int):
+    """ This function doesn't return anything, it just adds to the heap that part1 is processing
     """
-    1) for each situation, discover all legal moves and branch (am I thinking toward a recursive solve? yeah...)
-    2) but I also need previous states to be remembered, so repeated states mean fails
-    based off of Peter's code, I think I have a good path forward:
-    1) make a way to hash each setup, and save hashes to check for one we've seen before
-    2) don't do recursive. Use a prioritized heap instead.
-    I think I can use most of my current code to make this happen
-    """
-    # if test:
-    #     if steps > 0: print("forked!")
-    # check for solve
-    if test: check = 4
-    else: check = 10
-    if len(floors[4]) == check and floors[1] == [] and floors[2] == [] and floors[3] == []:
-        print(floors["inst"])
-        return steps
-    # check for creation of extra items
-    count = len(floors[4]) + len(floors[3]) + len(floors[2]) + len(floors[1])
-    if count != check:
-        # print(f"Error! Wrong number of items ({count})[91]!")
-        return 9999
-    # rather than check a history, just check if steps > 75
-    if steps > 50:
-        return 9999
-    # check if current conditions are legal; else, return 9999
-    if not check_valid(floors, check):
-        # if test: print("not valid!")
-        return 9999
-    """# check if the current layout exists in the history
-    # first limit to every layout of the same size
-    # make sure to check the value of E as well
-    if floors in hist:
-        return 9999"""
-    # save current layout to history
-    for i in range(1,5):
-        floors[i].sort()
     # test every single possible move
     ones = [[x] for x in floors[floors["E"]]]
     pairs = [list(x) for x in itertools.combinations([x for x in floors[floors["E"]]], 2)]
-    # if test: print(pairs)
     for i in pairs:
-        ones.append(i)
-    # if test: print(ones)
-    possible = []
+        if check_ele(i):
+            ones.append(i)
     for l in ones:
-        # if test: print(f"l: {l}")
         save = {"E":floors["E"],
-                "inst":floors["inst"],
                 1: floors[1].copy(),
                 2: floors[2].copy(),
                 3: floors[3].copy(),
@@ -79,41 +53,76 @@ def part1(floors: Dict, steps:int = 0, test:bool = False) -> int:
         up = floors["E"]+1
         down = floors["E"]-1
         up_floor = {"E":up,
-                    "inst":floors["inst"],
                     1: floors[1].copy(),
                     2: floors[2].copy(),
                     3: floors[3].copy(),
                     4: floors[4].copy()
                     }
         down_floor = {"E":down,
-                    "inst":floors["inst"],
                     1: floors[1].copy(),
                     2: floors[2].copy(),
                     3: floors[3].copy(),
                     4: floors[4].copy()
                     }
-        # given that I have the instructions saved, I should be able to make a check to prevent \
-        # undoing the move that just happened. Don't think this will really help my run times all that much though
-        if up < 5:
-            floors[up].append(i)
+        if up < 5 and len(l) > 1: # I'm pretty sure you always want to be moving up with two items, so let's try that
             up_floor["E"] = up
             for i in l:
-                up_floor[up].append(i)
+                up_floor[up].add(i)
             #check valid here
-            if check_valid(up_floor, check):
-                possible.append(up_floor)
-            up_floor["inst"] += f"{l} to {up}\n"
-        if down > 0:
+            if check_valid(up_floor): # make hash above and check here
+                h = hash_floors(up_floor)
+                if h not in hist:
+                    heap.append((count, up_floor))
+                    hist.add(h)
+                    # print(h)
+        if down > 0 and len(floors[1]): # no need to move down if floor 1 is empty
             down_floor["E"] = down
             for i in l:
-                down_floor[down].append(i)
+                down_floor[down].add(i)
             #check_valid here
-            if check_valid(down_floor, check):
-                possible.append(down_floor)
-            down_floor["inst"] += f"{l} to {down}\n"
+            if check_valid(down_floor): # make hash above and check here
+                h = hash_floors(down_floor)
+                if h not in hist:
+                    heap.append((count, down_floor))
+                    hist.add(h)
+                    # print(h)
         floors = save
-    # if test: print(possible)
-    return min(part1(x, steps+1, test) for x in possible)
+    
+
+def part1(floors: Dict, steps:int = 0, test:bool = False) -> int:
+    """
+    1) for each situation, discover all legal moves and branch (am I thinking toward a recursive solve? yeah...)
+    2) but I also need previous states to be remembered, so repeated states mean fails
+    based off of Peter's code, I think I have a good path forward:
+    1) make a way to hash each setup, and save hashes to check for one we've seen before
+        a) substep: convert to using sets instead of lists
+    2) don't do recursive. Use a prioritized heap instead. (Peter used heapq but I think I can make my own solution)
+    I think I can use most of my current code to make this happen
+    """
+    h_t = hash_floors(floors)
+    hist.add(h_t)
+    # hash test
+    # h_t2 = hash_floors(floors)
+    # if h_t == h_t2:
+    #     # print ("Success! hashes match")
+    #     print(h_t)
+    # else:
+    #     return 9900
+    # first, build the next steps
+    move_elevator(floors, 1)
+    last = 0
+    # check for solve
+    while heap:
+        popped = heap.pop(0)
+        c_f = popped[1]
+        if not c_f[1] and not c_f[2] and not c_f[3]:
+            return popped[0]
+        if popped[0] != last:
+            last = popped[0]
+            # print(f"floor: {last}; heap length: {len(heap)}")
+            heap_length.append(len(heap))
+        move_elevator(c_f, popped[0] + 1)
+    return 9999
 
 
 if __name__ == "__main__":
@@ -126,7 +135,15 @@ The third floor contains nothing relevant.
 The fourth floor contains nothing relevant."""
     """ repping the pairs as pos (gens) and neg (chips) nums; if a neg is on a floor w/o a corresponding
     positive, check for any other positive and throw error"""
-    DATA = {1: ["1G", "2G", "2M", "3G", "4G", "4M", "5G", "5M"], 2: ["1M", "3M"], 3: [], 4: [], "E": 1, "inst":""}
-    TEST_DATA = {"E": 1, 1: ["1M", "2M"], 2: ["1G"], 3: ["2G"], 4: [], "inst":""}
+    DATA = {1: {"1G", "2G", "2M", "3G", "4G", "4M", "5G", "5M"}, 2: {"1M", "3M"}, 3: set(), 4: set(), "E": 1}
+    TEST_DATA = {"E": 1, 1: ["1M", "2M"], 2: ["1G"], 3: ["2G"], 4: []}
     print(f"Part one: {part1(DATA, test=False)}") # not 25 (tried to solve mentally, forgot you can't run the elevator without holding anything), 
     # next manually calc solution of 83 was also wrong; also not 53 or 52
+    print(f"Max heap size: {max(heap_length)}; # of seen buildings: {len(hist)}")
+    DATA[1].update(["6G", "6M", "7G", "7M"])
+    hist = set()
+    heap = []
+    heap_length = []
+    print(DATA)
+    print(f"Part two: {part1(DATA, test=False)}") # getting 65 currently, which is wrong
+    print(f"Max heap size: {max(heap_length)}; # of seen buildings: {len(hist)}")
