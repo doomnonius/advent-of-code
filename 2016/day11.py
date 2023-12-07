d5c033b8224ed4b46a9344f42fa35cf3cfa856bb
Devon <devon.doornbos@emsibg.com>
2023-12-06 16:17:06 -0800

    know why Peters hash better; now to do own version

--- 2016/day11.py
+++ 2016/day11.py
@@ -1,13 +1,17 @@
 from typing import Dict, List, Tuple
 import itertools
+from datetime import datetime
 
 def hash_floors(floors: Dict) -> Tuple[str]:
+    # need to trash this one
     t = []
     for i in range(1,5):
-        p = sorted(floors[i].copy())
         if floors["E"] == i:
-            p.append("E")
-        t.append(tuple(p))
+            p = frozenset(floors[i] | {"E"})
+        else:
+            p = frozenset(floors[i])
+        # I definitely need to know which floor the elevator is on
+        t.append(tuple(sorted(tuple(p))))
     # print(tuple(t))
     return tuple(t)
 
@@ -32,61 +36,55 @@ heap = []
 hist = set()
 heap_length = []
 
-def move_elevator(floors:Dict,count:int):
+def move_elevator(floors:Dict,count:int,test:bool = False):
     """ This function doesn't return anything, it just adds to the heap that part1 is processing
     """
     # test every single possible move
-    ones = [[x] for x in floors[floors["E"]]]
-    pairs = [list(x) for x in itertools.combinations([x for x in floors[floors["E"]]], 2)]
+    f = floors["E"]
+    ones = [[x] for x in floors[f]]
+    pairs = [list(x) for x in itertools.combinations([x for x in floors[f]], 2)]
+    """ there are a lot of moves that are functionally equivalent - eg if you have three pairs of G+M on a floor,
+        the one you choose to move isn't actually a truly unique decision
+        or if you choose two Ms from those three, if also doesn't matter
+        just realized that's what I was confused about in Peter's hashing algorythm: he's converting to numbers
+        and I think I might be able to something similar
+    """
     for i in pairs:
         if check_ele(i):
             ones.append(i)
     for l in ones:
-        save = {"E":floors["E"],
-                1: floors[1].copy(),
-                2: floors[2].copy(),
-                3: floors[3].copy(),
-                4: floors[4].copy()
-                }
+        new_floors = {k:frozenset(v) for k,v in floors.items() if type(k) != str}
         for i in l:
-            floors[floors["E"]].remove(i)
-        up = floors["E"]+1
-        down = floors["E"]-1
-        up_floor = {"E":up,
-                    1: floors[1].copy(),
-                    2: floors[2].copy(),
-                    3: floors[3].copy(),
-                    4: floors[4].copy()
-                    }
-        down_floor = {"E":down,
-                    1: floors[1].copy(),
-                    2: floors[2].copy(),
-                    3: floors[3].copy(),
-                    4: floors[4].copy()
-                    }
+            new_floors[f] = new_floors[f] - {i}
+        up = f+1
+        down = f-1
+        up_floor= {k:frozenset(v) for k,v in new_floors.items()}
+        down_floor= {k:frozenset(v) for k,v in new_floors.items()}
         if up < 5 and len(l) > 1: # I'm pretty sure you always want to be moving up with two items, so let's try that
             up_floor["E"] = up
             for i in l:
-                up_floor[up].add(i)
+                up_floor[up] = up_floor[up] | {i}
             #check valid here
             if check_valid(up_floor): # make hash above and check here
                 h = hash_floors(up_floor)
                 if h not in hist:
                     heap.append((count, up_floor))
                     hist.add(h)
-                    # print(h)
+                    if test: print(h)
+                # else: print("layout found in history!")
         if down > 0 and len(floors[1]): # no need to move down if floor 1 is empty
             down_floor["E"] = down
             for i in l:
-                down_floor[down].add(i)
+                down_floor[down] = down_floor[down] | {i}
             #check_valid here
             if check_valid(down_floor): # make hash above and check here
                 h = hash_floors(down_floor)
                 if h not in hist:
                     heap.append((count, down_floor))
                     hist.add(h)
-                    # print(h)
-        floors = save
+                    if test: print(h)
+                # else: print("layout found in history!")
+        # floors = save
     
 
 def part1(floors: Dict, steps:int = 0, test:bool = False) -> int:
@@ -101,13 +99,6 @@ def part1(floors: Dict, steps:int = 0, test:bool = False) -> int:
     """
     h_t = hash_floors(floors)
     hist.add(h_t)
-    # hash test
-    # h_t2 = hash_floors(floors)
-    # if h_t == h_t2:
-    #     # print ("Success! hashes match")
-    #     print(h_t)
-    # else:
-    #     return 9900
     # first, build the next steps
     move_elevator(floors, 1)
     last = 0
@@ -117,9 +108,10 @@ def part1(floors: Dict, steps:int = 0, test:bool = False) -> int:
         c_f = popped[1]
         if not c_f[1] and not c_f[2] and not c_f[3]:
             return popped[0]
+        # this was added for tracking purposes
         if popped[0] != last:
             last = popped[0]
-            # print(f"floor: {last}; heap length: {len(heap)}")
+            if test: print(f"floor: {last}; heap length: {len(heap)}")
             heap_length.append(len(heap))
         move_elevator(c_f, popped[0] + 1)
     return 9999
@@ -137,7 +129,9 @@ The fourth floor contains nothing relevant."""
     positive, check for any other positive and throw error"""
     DATA = {1: {"1G", "2G", "2M", "3G", "4G", "4M", "5G", "5M"}, 2: {"1M", "3M"}, 3: set(), 4: set(), "E": 1}
     TEST_DATA = {"E": 1, 1: ["1M", "2M"], 2: ["1G"], 3: ["2G"], 4: []}
+    start = datetime.now()
     print(f"Part one: {part1(DATA, test=False)}") # not 25 (tried to solve mentally, forgot you can't run the elevator without holding anything), 
+    print(f"Solve time: {datetime.now()-start}")
     # next manually calc solution of 83 was also wrong; also not 53 or 52
     print(f"Max heap size: {max(heap_length)}; # of seen buildings: {len(hist)}")
     DATA[1].update(["6G", "6M", "7G", "7M"])
@@ -145,5 +139,7 @@ The fourth floor contains nothing relevant."""
     heap = []
     heap_length = []
     print(DATA)
-    print(f"Part two: {part1(DATA, test=False)}") # getting 65 currently, which is wrong
+    start = datetime.now()
+    print(f"Part two: {part1(DATA, test=True)}") # getting 65 currently, which is wrong
+    print(f"Solve time: {datetime.now()-start}")
     print(f"Max heap size: {max(heap_length)}; # of seen buildings: {len(hist)}")
