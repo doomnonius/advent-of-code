0a6a0067192a2d174c86642d7dae2b9c5ab8d375
Devon <devon.doornbos@emsibg.com>
2023-12-08 15:15:04 -0800

    solved 2016.d23.p2

--- 2016/day23.py
+++ 2016/day23.py
@@ -35,14 +35,12 @@ class Instruction:
 		# print("Toggled!")
 
 	def execute(self, d:Dict, inp:List, i, p2):
+		# optimizations
+		if i == 2:
+			d['a'] = d['a'] * d['b']
+			return 10
 		if self.command == "inc":
 			d[self.noun] += 1
-			return i + 1
-		elif self.command == "mul":
-			d[self.noun] *= d[self.verb]
-			return i + 1
-		elif self.command == "add":
-			d[self.noun] += d[self.verb]
 			return i + 1
 		elif self.command == "dec":
 			d[self.noun] -= 1
@@ -84,77 +82,27 @@ def part1(inp: List[Instruction], regs = {}, p2 = False) -> int:
 	i = 0
 	l = len(inp)
 	if not p2:
-		regs['a'] = 7
+		regs = {'a': 7, 'b': 0, 'c': 0, 'd': 0}
 	else:
-		regs['a'] = 12
+		regs= {'a': 12, 'b': 0, 'c': 0, 'd': 0}
 	
-	if p2: # I'm dumb, Peter would have had different instructions than me. I'm going to write some code to look
-		# loops, and then either manually shortcut or make the code shortcut for me
-		inp[2] = Instruction("mul a b")
-		for x in range(3,11):
-			inp[x] = Instruction("nop a b")
-		inp[21] = Instruction("add d a")
-		inp[22] = Instruction("nop a b")
-		inp[23] = Instruction("nop a b")
+	track = []
 
 	count = 0
 	while i < l:
 		i = inp[i].execute(regs, inp, i, p2)
-		if not count % 1000:
-			print(regs)
-
-	# toggled = set()
-	# while i < l:
-	# 	if inp[i][0:3] == "cpy" or inp[i][0:3] == "jnz":
-	# 		command, noun, verb = inp[i].split()
-	# 	else:
-	# 		command, noun, verb = inp[i].split() + ["z"]
-	# 	if isNumber(noun):
-	# 		noun = int(noun)
-	# 	else:
-	# 		if noun not in regs.keys():
-	# 			regs[noun] = 0
-	# 	if isNumber(verb):
-	# 		verb = int(verb)
-	# 	else:
-	# 		if verb not in regs.keys():
-	# 			regs[verb] = 0
-	# 	if (command == "cpy" and i not in toggled) or (command == "jnz" and i in toggled):
-	# 		try:
-	# 			regs[verb] = regs[noun]
-	# 		except KeyError:
-	# 			regs[verb] = noun
-	# 	elif (command == "inc" and i not in toggled) or (command == "dec" and i in toggled) or (command == "tgl" and i in toggled):
-	# 		regs[noun] += 1
-	# 	elif (command == "dec" and i not in toggled) or (command == "inc" and i in toggled):
-	# 		regs[noun] -= 1
-	# 	elif (command == 'jnz' and i not in toggled) or (command == "cpy" and i in toggled):
-	# 		# print(regs)
-	# 		if isNumber(verb):
-	# 			verb = int(verb)
-	# 		else:
-	# 			if verb not in regs.keys():
-	# 				regs[verb] = 0
-	# 			verb = regs[verb]
-	# 		try:
-	# 			if regs[noun] != 0:
-	# 				i += verb
-	# 				continue
-	# 		except KeyError:
-	# 			if noun != 0:
-	# 				i += verb
-	# 				continue
-	# 	elif command == "tgl" and i not in toggled:
-	# 		print(regs, toggled)
-	# 		try:
-	# 			hits = i + noun
-	# 		except:
-	# 			hits = i + regs[noun]
-	# 		if hits in toggled:
-	# 			toggled.remove(hits)
-	# 		else:
-	# 			toggled.add(hits)
-	# 	i += 1
+	# notes:
+	# i 2-9 is a = a * b
+	# instruction in i[10] to dec b is important
+	# i 11-15 is c = b * 2, then 16 toggles the instruction c away
+	# i 17 sets c to -16, then i 18 jumps us to i 2
+	# the last thing toggled away is 18 jumping us back down, when b = 1, c = 1 (because jnz is now copy), d = 0
+	# so by the time we have toggled i 18, 20, 22, 24
+	# 19 makes c 94
+	# we loop i 20-22, 20 makes d 82, and 21-23 a = a + d
+	# 24 decreases c
+	# 25 jumps based on c
+	# a += so d (82) * c (94) times
 	return regs
 
 
@@ -175,5 +123,5 @@ if __name__ == "__main__":
 	# DATA = DATA.split("\n")
 	print(f"Part one: {part1(DATA)['a']}")
 	DATA = [Instruction(x) for x in RAW_DATA.split("\n")]
-	print(f"Part two: {part1(DATA, p2 = True)['a']}") # not 3636508, too low; something around 479,001,600
+	print(f"Part two: {part1(DATA, p2 = True)['a']}") # not 3636508, too low; 479,001,600 + (94*82) = 479009308
 	# print(f"Time: {timeit.timeit('', setup='from __main__ import ', number = 1)}")
