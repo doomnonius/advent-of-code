aaee34b137d16bba38fbc900d809e5d5f06ce2f4
Devon <devon.doornbos@emsibg.com>
2024-01-05 03:28:58 -0800

    changes to execution code

--- 2023/day25.py
+++ 2023/day25.py
@@ -14,10 +14,12 @@ def part2(nums: List[int]) -> int:
 
 
 if __name__ == "__main__":
-	import os, timeit
-	from pathlib import Path
-	INPUT_FILE = Path(__file__).with_suffix(".input")
-	DATA = INPUT_FILE.read_text().strip()
+	from datetime import datetime
+    from pathlib import Path
+    test = True
+    if test: INPUT_FILE = Path(__file__).with_suffix(".testinput")
+    else: INPUT_FILE = Path(__file__).with_suffix(".input")
+    RAW_DATA = INPUT_FILE.read_text().strip().split("\n")
 	DATA = [int(x) for x in DATA.split()] # example code
 	print(f"Part 1: {part1(DATA)}")
 	print(f"Part 2: {part2(DATA)}")
