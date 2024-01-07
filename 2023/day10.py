7b7a79c90edf955264aa7ce139c970ad9c89e88d
Devon <devon.doornbos@emsibg.com>
2024-01-07 14:16:16 -0800

    solved 2023.d17.p2 with help from Clint's solution

--- 2023/day10.py
+++ 2023/day10.py
@@ -10,6 +10,15 @@ class Coord:
         # if not other: return False
         return self.x == other.x and self.y == other.y
 
+    def __add__(self, other):
+        return Coord(self.x + other.x, self.y + other.y)
+
+    def __lt__(self, other):
+        return self.x + self.y < other.x + other.y
+    
+    def __gt__(self, other):
+        return self.x + self.y > other.x + other.y
+
     def __hash__(self):
         return hash((self.x, self.y))
     
--- 2023/day17.py
+++ 2023/day17.py
@@ -1,65 +1,51 @@
 from typing import List, Dict, Tuple
+import heapq
 from day10 import Coord
 
-def map_to_mid(data: Dict[Coord, int], min_move: int, max_move: int, cut: int) -> Dict:
+def part1(data: Dict[Coord, int], min_move: int, max_move: int) -> Dict:
     
-    def find_next(loc: Coord, dir: int, streak: int, total: int):
+    def find_next(loc: Coord, dir: int, total: int):
         r = []
         turn_d: Dict[int,List[Tuple[Coord, int]]] = {
-            1 : [(loc.up(), 0), (loc.right(), 1), (loc.down(), 2)],
-            2 : [(loc.right(), 1), (loc.down(), 2), (loc.left(), 3)],
-            3 : [(loc.down(), 2), (loc.left(), 3), (loc.up(), 0)],
-            0 : [(loc.left(), 3), (loc.up(), 0), (loc.right(), 1)]
+            1 : [(loc.up(), 0), (loc.down(), 2)],
+            2 : [(loc.right(), 1), (loc.left(), 3)],
+            3 : [(loc.down(), 2), (loc.up(), 0)],
+            0 : [(loc.left(), 3), (loc.right(), 1)]
+        }
+        str_d: Dict[int,Coord] = {
+            1 : Coord(1,0),
+            2 : Coord(0,1),
+            3 : Coord(-1,0),
+            0 : Coord(0,-1),
         }
         for x in turn_d[dir]:
-            n, d = x
-            if d == dir:
-                if streak == max_move:
+            # give all possible moves turning from this spot
+            next_loc, next_dir = x
+            next_heat = 0
+            for i in range(1, max_move + 1):
+                if next_loc not in data:
                     continue
-                else:
-                    new_streak = streak + 1
-            else:
-                if streak < min_move:
-                    continue
-                new_streak = 1
-            if n not in min_d:
-                min_d[n] = dict()
-            if (new_streak, d) not in min_d[n]:
-                min_d[n].update({(new_streak, d):10000})
-            if n in data:
-                new_total = total + data[n]
-                if new_total < min_d[n][(new_streak, d)]:
-                    min_d[n][(new_streak, d)] = new_total
-                    r.append((n, d, new_streak, new_total))
+                next_heat += data[next_loc]
+                if i >= min_move:
+                    r.append((total + next_heat, next_loc, next_dir))
+                next_loc += str_d[next_dir]
+
         return r
 
-    min_d: Dict[Coord, Dict[Tuple[int, int], int]] = {}
     target = list(data.keys())[-1]
-    # print(f"target: {target}")
-    stack: List[Tuple[Coord, int, int, int]] = find_next(Coord(0,0), 1, 1, 0)
-    solutions = []
-    test = 0
-    new_stack: List[Tuple[Coord, int, int, int]] = []
-    while True:
-        if not stack:
-            break
-        while stack:
-            loc, d, streak, total = stack.pop(0)
-            if loc == target and streak >= min_move:
-                solutions.append(total)
-            for x in find_next(loc, d, streak, total):
-                new_stack.append(x)
-        new_stack.sort(key=lambda x: x[3])
-        stack = new_stack[0:cut]
-        new_stack = []
-    return min(v for k,v in min_d[target].items() if k[0] >= min_move)
-
-
-
-def part1(data: Dict[Coord, int], min_move: int, max_move: int, cut: int) -> int:
-    mid = len(data) // 2
-
-
+    heap = find_next(Coord(0,0), 1, 0)
+    for x in find_next(Coord(0,0), 2, 0):
+        heapq.heappush(heap, x)
+    seen = set()
+    while heap:
+        total, loc, d = heapq.heappop(heap)
+        if (loc, d) in seen:
+            continue
+        seen.add((loc, d))
+        if loc == target: #and streak >= min_move:
+            return total
+        for x in find_next(loc, d, total):
+            heapq.heappush(heap, x)
 
 
 if __name__ == "__main__":
@@ -73,6 +59,5 @@ if __name__ == "__main__":
     for row in range(len(RAW_DATA)):
         for col in range(len(RAW_DATA[row])):
             DATA[Coord(col,row)] = int(RAW_DATA[row][col])
-    # print(DATA)
-    print(f"Part 1: {part1(DATA, 1, 3, 1000)}")
-    print(f"Part 2: {part1(DATA, 4, 10, 1000000000)}") # not 1398, too high; 1320 also; 1310 also; 1286 is wrong
+    print(f"Part 1: {part1(DATA, 1, 3)}")
+    print(f"Part 2: {part1(DATA, 4, 10)}") # not 1398, too high; 1320 also; 1310 also; 1286 is wrong
