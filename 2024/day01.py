1234ffbf119cc517818e6cd64063d6921fd62363
Devon <devon.doornbos@emsibg.com>
2024-12-19 11:03:01 -0800

    progress on 2024.d01

--- 2024/day01.py
+++ 2024/day01.py
@@ -1,33 +1,41 @@
 from typing import List, Tuple
 
-def part1(nums: List[int]) -> int:
-    return
+def part1(nums: Tuple[List[str]]) -> int:
+    l0 = sorted(nums[0])
+    l1 = sorted(nums[1])
+    r = 0
+    for i in range(0,len(l1)):
+        r += abs(l0[i] - l1[i])
+    return r
 
-
-
-
-
-def part2(nums: List[int]) -> int:
-    return
+def part2(nums: Tuple[List[str]]) -> int:
+    r = 0
+    l0 = set(nums[0])
+    l1 = sorted(nums[1])
+    for n in l0:
+        r += (n * l1.count(n))
+    return r
 
 
 def process_data(data: str) -> Tuple[List[str]]:
-    lines = data.strip().split("\n")
+    l0 = []
     l1 = []
-    l2 = []
-    [l1.append(x) and l2.append(y) for line in lines.split("\n") for x,y in line.split()]
-    return (l1, l2)
+    for line in data.split("\n"):
+        d = line.split()
+        l0.append(int(d[0]))
+        l1.append(int(d[1]))
+    return (l0, l1)
 
 if __name__ == "__main__":
     from datetime import datetime
     from pathlib import Path
-    from .helpers import import_files
+    from helpers import import_files
     TEST_FILE, INPUT_FILE = import_files(Path(__file__))
     TEST_DATA = process_data(TEST_FILE)
     DATA = process_data(INPUT_FILE)
     test_1a = 11
-    test_2a = 0
-    if test_1a: assert part1(TEST_DATA) == test_1a
+    test_2a = 31
+    if test_1a: assert part1(TEST_DATA) == test_1a, f""
     print(f"Part 1: {part1(DATA)}")
-    if test_2a: assert part2(TEST_DATA) == test_2a
+    if test_2a: assert part2(TEST_DATA) == test_2a, f""
     print(f"Part 2: {part2(DATA)}")
--- 2024/helpers.py
+++ 2024/helpers.py
@@ -1,6 +1,6 @@
 from typing import Tuple
 
-def import_files(day: str) -> Tuple[str, str]:
-    TEST_FILE = day + ".testinput".read_text()
-    INPUT_FILE = day + ".input".read_text()
+def import_files(day) -> Tuple[str, str]:
+    TEST_FILE = day.with_suffix(".testinput").read_text()
+    INPUT_FILE = day.with_suffix(".input").read_text()
     return TEST_FILE, INPUT_FILE
