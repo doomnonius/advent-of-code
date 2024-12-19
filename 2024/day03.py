762f0402d60d62aeac48648b16a8ff9b8a564ed8
Devon <devon.doornbos@emsibg.com>
2024-12-19 14:22:16 -0800

    solved 2024.d03

--- 2024/day03.py
+++ 2024/day03.py
@@ -1,21 +1,31 @@
 from typing import List
+import re
 
-def part1(nums: List[int]) -> int:
-    return
+def part1(nums: List[str]) -> int:
+    r = 0
+    pat = r'mul\((\d{1,3}),(\d{1,3})\)'
+    for s in nums:
+        for m in re.findall(pat, s):
+            r += (int(m[0]) * int(m[1]))
+    return r
 
+def part2(nums: List[str]) -> int:
+    r = 0
+    pat = r'do\(\)|mul\((\d{1,3}),(\d{1,3})\)|don\'t\(\)'
+    on = True
+    nums = "".join(nums)
+    for m in re.finditer(pat, nums):
+        if m.group(0) == "do()":
+            on = True
+        elif m.group(0) == "don't()":
+            on = False
+        else:
+            if on: r += (int(m.group(1)) * int(m.group(2)))
+    return r
 
-
-
-
-def part2(nums: List[int]) -> int:
-    return
-
-
-
-
-def process_data(data: str) -> Tuple[List[str]]:
+def process_data(data: str) -> List[str]:
     lines = data.strip().split("\n")
-    return
+    return lines
 
 if __name__ == "__main__":
     from datetime import datetime
@@ -24,8 +34,8 @@ if __name__ == "__main__":
     TEST_FILE, INPUT_FILE = import_files(Path(__file__))
     TEST_DATA = process_data(TEST_FILE)
     DATA = process_data(INPUT_FILE)
-    test_1a = 0
-    test_2a = 0
+    test_1a = 161
+    test_2a = 48
     p1 = part1(TEST_DATA)
     p2 = part2(TEST_DATA)
     if test_1a: assert p1 == test_1a, f"Part 1: {p1} is the wrong answer"
