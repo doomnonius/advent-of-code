28d52ed81ac98cb5c17bb854131a8e4aef91ae15
Devon <devon.doornbos@emsibg.com>
2024-12-19 13:40:55 -0800

    solved 2024.d02

--- 2024/day02.py
+++ 2024/day02.py
@@ -1,19 +1,49 @@
 from typing import List
 
-def part1(nums: List[List[int]], damp=False) -> int:
+def part1(nums: List[List[int]], t=False) -> int:
     r = 0
     for row in nums:
         if row not in [sorted(row), sorted(row, reverse=True)]:
             continue
         safe = True
-        danger = 0
         for i in range(len(row)-1):
             if not 0 < abs(row[i+1]-row[i]) < 4:
-                danger += 1
-                if danger > 1: safe = False
+                safe = False
         r += safe
     return r
 
+
+def part2(nums: List[List[int]], t=False) -> int:
+    r = 0
+    for row in nums:
+        safe = eval_row(row, False, t)
+        r += safe
+    return r
+
+def eval_row(r: List[int], skipped: bool, t=False) -> bool:
+    if (len(r) - len(set(r))) > 1:
+        return False
+    change = 0
+    for i in range(len(r)-1):
+        a = r[i]
+        b = r[i+1]
+        if not change:
+            if not i:
+                change = b-a
+                continue
+            elif not skipped:
+                return eval_row(r[1:], True, t)
+            else:
+                return False
+        elif (-4 < change < 0 and -4 < b-a < 0) or (4 > change > 0 and 4 > b-a > 0):
+            change = b-a
+        elif not skipped:
+            if eval_row(r[:i] + r[i+1:], True, t) or eval_row(r[:i-1] + r[i:], True, t) or eval_row(r[:i+1] + r[i+2:], True, t):
+                return True
+            return False
+        else:
+            return False
+    return True
 
 def process_data(data: str) -> List[List[int]]:
     r = []
@@ -30,11 +60,11 @@ if __name__ == "__main__":
     DATA = process_data(INPUT_FILE)
     test_1a = 2
     test_2a = 4
-    p1 = part1(TEST_DATA)
-    p2 = part1(TEST_DATA, True)
+    p1 = part1(TEST_DATA, True)
     if test_1a: assert p1 == test_1a, f"Part 1: {p1} is the wrong answer"
     p1 = part1(DATA)
     print(f"Part 1: {p1}")
+    p2 = part2(TEST_DATA, True)
     if test_2a: assert p2 == test_2a, f"Part 2: {p2} is the wrong answer"
-    p2 = part1(DATA,True)
+    p2 = part2(DATA, True) # too low: 660 or 668; too high: 689; not 678
     print(f"Part 2: {p2}")
