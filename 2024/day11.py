342aece64e0b1d052032188a1411dde648134a36
Devon <devon.doornbos@emsibg.com>
2025-01-03 07:49:07 -0800

    2024.d11 optimizations in progress

--- 2024/day11.py
+++ 2024/day11.py
@@ -1,21 +1,49 @@
-from typing import List
+from typing import List, Dict, Tuple
 
-def part1(nums: List[int]) -> int:
-    return
+class Rock:
+    def __init__(self, n, d=0):
+        self.data = n
+        self.left = None
+        self.right = None
+        self.depth = d
+    
+    def blink(self, rocks: Dict) -> Tuple:
+        if not self.data:
+            self.left = Rock(1, self.depth + 1)
+            return
+        l = len(str(self.data))
+        if l % 2 == 0:
+            self.left = Rock(int(self.data//(10 ** (l/2))), self.depth + 1)
+            self.right = Rock(int(self.data - self.left.data * (10 ** (l/2))), self.depth + 1)
+            return
+        self.left = Rock(self.data * 2024, self.depth + 1)
+        return (x for x in [self.left, self.right] if x)
+    
+    def leng(self):
+        pass
+    
+    def __hash__(self):
+        return hash(self.data)
+    
+
+def part1(rocks: List[Rock], w=25, t=False) -> int:
+    seen: Dict[int, Rock] = dict()
+    r = 0
+    for r in rocks:
+        seen[r.data] = r
+        c = w
+        while c > 0:
+            for r in rocks:
+                for s in r.blink(seen):
+                    seen[s.data] = s
+            c -= 1
+        r += r.leng()
+    return r
 
 
-
-
-
-def part2(nums: List[int]) -> int:
-    return
-
-
-
-
-def process_data(data: str) -> Tuple[List[str]]:
-    lines = data.strip().split("\n")
-    return
+def process_data(data: str) -> List[Rock]:
+    nums = [Rock(int(x)) for x in data.strip().split()]
+    return nums
 
 if __name__ == "__main__":
     from datetime import datetime
@@ -24,13 +52,11 @@ if __name__ == "__main__":
     TEST_FILE, INPUT_FILE = import_files(Path(__file__))
     TEST_DATA = process_data(TEST_FILE)
     DATA = process_data(INPUT_FILE)
-    test_1a = 0
+    test_1a = 55312
     test_2a = 0
     p1 = part1(TEST_DATA)
-    p2 = part2(TEST_DATA)
     if test_1a: assert p1 == test_1a, f"Part 1: {p1} is the wrong answer"
     p1 = part1(DATA)
     print(f"Part 1: {p1}")
-    if test_2a: assert p2 == test_2a, f"Part 2: {p2} is the wrong answer"
-    p2 = part2(DATA)
+    p2 = part1(DATA, 75)
     print(f"Part 2: {p2}")
